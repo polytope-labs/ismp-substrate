618a63617aadbd664054beb74312bd058bd4ccc6
Damilare <dharjeezy@gmail.com>
2023-03-24 18:34:20 +0100

    is_frozen check and timestamp changes

--- src/beacon_consensus_client.rs
+++ src/beacon_consensus_client.rs
@@ -1,5 +1,4 @@
 use codec::{Decode, Encode};
-use frame_support::traits::UnixTime;
 use ismp_rust::consensus_client::{ConsensusClient, ConsensusClientId, ETHEREUM_CONSENSUS_CLIENT_ID, IntermediateState, StateCommitment, StateMachineHeight, StateMachineId};
 use ismp_rust::error::Error;
 use ismp_rust::host::ISMPHost;
@@ -30,19 +29,15 @@ const UNBONDING_PERIOD: u64 = 14;
 const DAY: u64 = 24 * 60 * 60;
 const EXECUTION_PAYLOAD_STATE_ID:u64 = 1;
 
-pub struct EthConsensusClient;
-
 impl ConsensusClient for ConsensusState {
     fn verify(
         &self,
         host: &dyn ISMPHost,
         trusted_consensus_state: Vec<u8>,
-        proof: Vec<u8>, // The light client update is the proof, use sync committee rs to verify
+        proof: Vec<u8>,
     ) -> Result<(Vec<u8>, Vec<IntermediateState>), Error> {
-        //TODO: create proper error type for it in ISMP Rust
-
         let beacon_message = BeaconMessage::decode(&mut &proof[..])
-            .map_err(|_| Error::CannotHandleConsensusMessage)?;
+            .map_err(|_| Error::ImplementationSpecific(format!("Cannot decode beacon message {:?}", proof)))?;
 
         let light_client_update = match beacon_message {
             BeaconMessage::ConsensusUpdate(update) => update.clone(),
@@ -50,17 +45,11 @@ impl ConsensusClient for ConsensusState {
         };
 
         let light_client_state = LightClientState::decode(&mut &trusted_consensus_state[..])
-            .map_err(|_| Error::CannotHandleConsensusMessage)?;
+            .map_err(|_| Error::ImplementationSpecific(format!("Cannot decode trusted consensus state {:?}", trusted_consensus_state)))?;
 
         let height = light_client_update.finalized_header.slot;
         // Ensure consensus client is not frozen
-        let is_frozen = if let Some(frozen_height) = self.frozen_height {
-            light_client_update.finalized_header.slot >= frozen_height
-        } else {
-            false
-        };
-
-        if is_frozen {
+        if host.host_timestamp() - host.consensus_update_time(self.consensus_id())? >=  self.unbonding_period() {
             return Err(Error::FrozenConsensusClient {
                 id: self.consensus_id(),
             });
@@ -72,15 +61,15 @@ impl ConsensusClient for ConsensusState {
             // return the right error, need to update ismp_rust
         }
 
-        let no_codec_light_client_state = light_client_state
+        let no_codec_light_client_state = light_client_state.clone()
             .try_into()
-            .map_err(|_| Error::CannotHandleConsensusMessage)?;
+            .map_err(|_| Error::ImplementationSpecific(format!("Cannot convert light client state {:?} to no codec type", light_client_state)))?;
         let no_codec_light_client_update = light_client_update
             .clone()
             .try_into()
-            .map_err(|_| Error::CannotHandleConsensusMessage)?;
+            .map_err(|_|  Error::ImplementationSpecific(format!("Cannot convert light client update {:?} to no codec type", light_client_update)))?;
 
-        let new_light_client_state = sync_committee_verifier::verify_sync_committee_attestation(
+        let _new_light_client_state = sync_committee_verifier::verify_sync_committee_attestation(
             no_codec_light_client_state,
             no_codec_light_client_update,
         )
@@ -114,23 +103,23 @@ impl ConsensusClient for ConsensusState {
 
     fn verify_membership(
         &self,
-        host: &dyn ISMPHost,
-        key: Vec<u8>,
-        commitment: Vec<u8>,
+        _host: &dyn ISMPHost,
+        _key: Vec<u8>,
+        _commitment: Vec<u8>,
     ) -> Result<(), Error> {
         todo!()
     }
 
     fn verify_non_membership(
         &self,
-        host: &dyn ISMPHost,
-        key: Vec<u8>,
-        commitment: Vec<u8>,
+        _host: &dyn ISMPHost,
+        _key: Vec<u8>,
+        _commitment: Vec<u8>,
     ) -> Result<(), Error> {
         todo!()
     }
 
-    fn is_frozen(&self, host: &dyn ISMPHost, id: ConsensusClientId) -> Result<bool, Error> {
+    fn is_frozen(&self, _host: &dyn ISMPHost, _id: ConsensusClientId) -> Result<bool, Error> {
         todo!()
     }
 }
