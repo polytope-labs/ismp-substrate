d0cf8ef17a4fbca73b23bc0efdc60c42751acbea
Damilare <dharjeezy@gmail.com>
2023-07-08 15:12:25 +0100

    changes as regards consensus states

--- grandpa/src/lib.rs
+++ grandpa/src/lib.rs
@@ -27,13 +27,13 @@ pub mod pallet {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
 
-    /// Mapping of standalone chain consensus state to 1 state machine.
+    /// Mapping of standalone chain consensus state id to 1 state machine.
     #[pallet::storage]
     #[pallet::getter(fn relay_chain_state)]
     pub type StandaloneChainConsensusState<T: Config> =
     StorageMap<_, Blake2_128Concat, Vec<u8>, StateMachine>;
 
-    /// Mapping of relay chain consensus state to set of para ids.
+    /// Mapping of relay chain consensus state id to set of para ids.
     #[pallet::storage]
     #[pallet::getter(fn relay_chain_state)]
     pub type RelayChainConsensusState<T: Config> =
@@ -59,22 +59,20 @@ pub mod pallet {
         /// Add some new parachains to the list of parachains in the relay chain consensus state
         #[pallet::call_index(0)]
         #[pallet::weight(0)]
-        pub fn add_parachain_to_relay_chain_consensus_state(origin: OriginFor<T>, consensus_state_id: Vec<u8>, para_ids: Vec<u32>) -> DispatchResult {
+        pub fn add_parachains(origin: OriginFor<T>, consensus_state_id: Vec<u8>, para_ids: Vec<u32>) -> DispatchResult {
             ensure_root(origin)?;
 
-            for para_id in para_ids {
-                RelayChainConsensusState::<T>::mutate(&consensus_state_id, |state_set| {
-                    if state_set.is_none() {
-                        let mut new_set = BTreeSet::new();
-                        new_set.insert(para_id);
-                        *state_set = Some(new_set);
+            RelayChainConsensusState::<T>::mutate(&consensus_state_id, |state_set| {
+                para_ids.iter().for_each(|para_id| {
+                    if let Some(set) = state_set {
+                        set.insert(*para_id);
                     } else {
-                        let mut existing_set = state_set.take().unwrap();
-                        existing_set.insert(para_id);
-                        *state_set = Some(existing_set);
+                        let mut new_set = BTreeSet::new();
+                        new_set.insert(*para_id);
+                        *state_set = Some(new_set);
                     }
                 });
-            }
+            });
 
             Ok(())
         }
@@ -82,18 +80,18 @@ pub mod pallet {
         /// Remove some parachains from the list of parachains in the relay chain consensus state
         #[pallet::call_index(1)]
         #[pallet::weight(0)]
-        pub fn remove_parachain_from_relay_chain_consensus_state(origin: OriginFor<T>, para_ids: Vec<u32>) -> DispatchResult {
+        pub fn remove_parachains(origin: OriginFor<T>, para_ids: Vec<u32>) -> DispatchResult {
             ensure_root(origin)?;
-            for para_id in para_ids {
-                RelayChainConsensusState::<T>::mutate_exists(&para_id, |state_set| {
+            para_ids.iter().for_each(|para_id| {
+                RelayChainConsensusState::<T>::mutate_exists(para_id, |state_set| {
                     if let Some(state_set) = state_set {
-                        state_set.remove(&para_id);
+                        state_set.remove(para_id);
                         if state_set.is_empty() {
                             *state_set = None;
                         }
                     }
                 });
-            }
+            });
 
             Ok(())
         }
@@ -101,23 +99,8 @@ pub mod pallet {
         /// Add state machine to standalone consensus state storage
         #[pallet::call_index(2)]
         #[pallet::weight(0)]
-        pub fn add_standalone_consensus(origin: OriginFor<T>, consensus_state_id: Vec<u8>, state_machine: StateMachine) -> DispatchResult {
+        pub fn set_state_machine(origin: OriginFor<T>, consensus_state_id: Vec<u8>, state_machine: StateMachine) -> DispatchResult {
             ensure_root(origin)?;
-
-            ensure!(!StandaloneChainConsensusState::<T>::contains_key(&consensus_state_id), Error::<T>::StandaloneConsensusStateAlreadyExists);
-
-            StandaloneChainConsensusState::<T>::insert(&consensus_state_id, state_machine);
-
-            Ok(())
-        }
-
-        /// Add state machine to standalone consensus state storage
-        #[pallet::call_index(3)]
-        #[pallet::weight(0)]
-        pub fn remove_standalone_consensus_state(origin: OriginFor<T>, consensus_state_id: Vec<u8>) -> DispatchResult {
-            ensure_admin_origin(origin)?;
-
-            ensure!(StandaloneChainConsensusState::<T>::contains_key(&consensus_state_id), Error::<T>::StandaloneConsensusStateDontExists);
 
             StandaloneChainConsensusState::<T>::insert(&consensus_state_id, state_machine);
 
