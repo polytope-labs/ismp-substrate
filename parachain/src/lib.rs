c4b5fe006e8437a3469a5a2a4953fc580b934371
Seun Lanlege <seunlanlege@gmail.com>
2023-07-25 21:11:31 +0100

    auto initialize parachain consensus client

--- parachain/src/lib.rs
+++ parachain/src/lib.rs
@@ -26,9 +26,10 @@ pub mod consensus;
 
 use alloc::{vec, vec::Vec};
 use cumulus_primitives_core::relay_chain;
-use ismp::handlers;
+use ismp::{handlers, messaging::CreateConsensusState};
 pub use pallet::*;
 use pallet_ismp::host::Host;
+use primitive_types::H256;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -36,9 +37,11 @@ pub mod pallet {
     use cumulus_primitives_core::relay_chain;
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use ismp::messaging::{ConsensusMessage, CreateConsensusState, Message};
+    use ismp::{
+        host::IsmpHost,
+        messaging::{ConsensusMessage, Message},
+    };
     use parachain_system::{RelaychainDataProvider, RelaychainStateProvider};
-    use primitive_types::H256;
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
@@ -129,7 +132,10 @@ pub mod pallet {
 
     // Pallet implements [`Hooks`] trait to define some logic to execute in some context.
     #[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
+    where
+        <T as frame_system::Config>::Hash: From<H256>,
+    {
         fn on_finalize(_n: T::BlockNumber) {
             let state = RelaychainDataProvider::<T>::current_relay_chain_state();
             if !RelayChainState::<T>::contains_key(state.number) {
@@ -147,6 +153,11 @@ pub mod pallet {
         fn on_initialize(_n: T::BlockNumber) -> Weight {
             // kill the storage, since this is the beginning of a new block.
             ConsensusUpdated::<T>::kill();
+
+            let host = Host::<T>::default();
+            if let Err(_) = host.consensus_state(consensus::PARACHAIN_CONSENSUS_ID) {
+                Pallet::<T>::initialize(host);
+            }
 
             Weight::from_parts(0, 0)
         }
@@ -197,18 +208,8 @@ pub mod pallet {
     {
         fn build(&self) {
             let host = Host::<T>::default();
+            Pallet::<T>::initialize(host);
 
-            let message = CreateConsensusState {
-                // insert empty bytes
-                consensus_state: vec![],
-                unbonding_period: u64::MAX,
-                challenge_period: 0,
-                consensus_state_id: consensus::PARACHAIN_CONSENSUS_ID,
-                consensus_client_id: consensus::PARACHAIN_CONSENSUS_ID,
-                state_machine_commitments: vec![],
-            };
-            handlers::create_client(&host, message)
-                .expect("Failed to initialize parachain consensus client");
             // insert the parachain ids
             for id in &self.parachains {
                 Parachains::<T>::insert(id, ());
@@ -217,11 +218,31 @@ pub mod pallet {
     }
 }
 
-impl<T: Config> Pallet<T> {
+impl<T: Config> Pallet<T>
+where
+    <T as frame_system::Config>::Hash: From<H256>,
+{
     /// Returns the list of parachains who's consensus updates will be inserted by the inherent
     /// data provider
     pub fn para_ids() -> Vec<u32> {
         Parachains::<T>::iter_keys().collect()
+    }
+
+    /// Initializes the parachain consensus state. Rather than requiring a seperate
+    /// `create_consensus_state` call, simply including this pallet in your runtime will create the
+    /// ismp parachain client consensus state, either through `genesis_build` or `on_initialize`.
+    pub fn initialize(host: Host<T>) {
+        let message = CreateConsensusState {
+            // insert empty bytes
+            consensus_state: vec![],
+            unbonding_period: u64::MAX,
+            challenge_period: 0,
+            consensus_state_id: consensus::PARACHAIN_CONSENSUS_ID,
+            consensus_client_id: consensus::PARACHAIN_CONSENSUS_ID,
+            state_machine_commitments: vec![],
+        };
+        handlers::create_client(&host, message)
+            .expect("Failed to initialize parachain consensus client");
     }
 }
 
